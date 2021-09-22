9f60fa3d011f8e7f8c467277222ff01b2088533a
Alex <hazerot.work@yandex.ru>
2021-09-22 19:03:17 +0300

    Update show_invoke_bs_count.py

--- Plugin/show_invoke_bs_count.py
+++ Plugin/show_invoke_bs_count.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # vim:fileencoding=utf-8
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fileencoding=utf-8
 import sublime
 import sublime_plugin
 import re
@@ -12,8 +15,7 @@ class ShowInvokeBsCountCommand(sublime_plugin.WindowCommand):
         v = self.window.active_view()
         resultStr = ""
         startTime = datetime.datetime.now()
-        arrSvcName = ['']
-        arrSvcCount = [0]
+        myDict = {}
         regions = v.find_all(r'Begin: Business Service')#, sublime.IGNORECASE,"\2:")
         for region in regions:
             for line in v.lines(region):
@@ -21,16 +23,17 @@ class ShowInvokeBsCountCommand(sublime_plugin.WindowCommand):
                 str2 = re.findall(r'\'[\(\)\#\d\s\w]+\'', s)
                 svcItem = "BS: " + str2[0] + " METHOD: " + str2[1]
 
-                if svcItem in arrSvcName:
-                    index = arrSvcName.index(svcItem)
-                    arrSvcCount[index] = arrSvcCount[index] + 1
+                if svcItem in myDict:
+                    myDict[svcItem] = myDict[svcItem] + 1
                 else:
-                    arrSvcName.append(svcItem)
-                    index = arrSvcName.index(svcItem)
-                    arrSvcCount.insert(index, 1)
+                    myDict[svcItem] = 1
 
-        for i in arrSvcName:
-            resultStr = resultStr + str(i) + "    COUNT = " + str(arrSvcCount[arrSvcName.index(i)]) + "\n"
+        sortedDict = {}
+        sortedKeys = sorted(myDict, key=myDict.get)
+        for w in sortedKeys:
+            sortedDict[w] = myDict[w]
+        for i in sortedDict.keys():
+            resultStr = resultStr + i + "    COUNT = " + str(sortedDict[i]) + "\n"
         if resultStr:
             newView = v.window().new_file()
             newView.run_command("insert",{"characters": resultStr})
